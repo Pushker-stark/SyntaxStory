f9c03fa416fb7e2f44aea1b6efa680f7259489c4
Pushker-stark <pushkernaresh0607@gmail.com>
2024-12-01 22:48:55 +0530

    ready for deploy with route fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,30 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Signup from './pages/Signup'
-import Signin  from './pages/Signin'
-import Blog  from './pages/Blog'
-import Blogs  from "./pages/Blogs";
-import Publish  from './pages/Publish';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Blog from './pages/Blog';
+import Blogs from "./pages/Blogs";
+import Publish from './pages/Publish';
 
 function App() {
-  // console.log(import.meta.env.VITE_API_URL);
+  const token = localStorage.getItem("token"); // Check if there's a JWT token in localStorage
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish />} />
-          {/* Default Route */}
-          <Route path="*" element={<Navigate to="/signin" />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/publish" element={<Publish />} />
+        
+        {/* If not authenticated, redirect to signin */}
+        <Route path="/" element={token ? <Navigate to="/blogs" /> : <Navigate to="/signin" />} />
+        
+        {/* Default Route */}
+        <Route path="*" element={token ? <Navigate to="/blogs" /> : <Navigate to="/signin" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
